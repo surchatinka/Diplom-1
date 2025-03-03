219745a70bf386e3227ffc35ca08b81765d657ac
Efim Klaus <nuclearmarmot@yandex.ru>
2025-03-03 10:48:25 +0300

    add test class for IngredientType

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.IngredientType;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+
+    private final IngredientType ingredientType;
+
+    public IngredientTypeTest(IngredientType type){
+        this.ingredientType = type;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] testData(){
+        return new Object[][]{
+                {IngredientType.SAUCE},
+                {IngredientType.FILLING}
+        };
+    }
+
+    @Test
+    public void checkIngredientTypeIsExist_ok() {
+        Assert.assertNotNull(ingredientType);
+    }
+
+}
